0c10e2a9e1bdfa3b65d22acf3faba4a5fb240ae4
Devils <pijiaxin@wsdashi.com>
2025-03-06 16:01:40 +0800

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
-  resolve:{
-    alias:{
-      '@': path.resolve(__dirname,'src')
-    }
-  }
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
